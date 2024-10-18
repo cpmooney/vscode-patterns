d2dc49d01304c0d0cd87c72ab8b6cadbb9333a0b
Chris Mooney <89551217+cpmooney@users.noreply.github.com>
2024-10-18 16:25:11 -0500

    Got it mostly working

--- src/extension.ts
+++ src/extension.ts
@@ -6,33 +6,52 @@ export function activate(context: vscode.ExtensionContext) {
 		context.subscriptions.push(vscode.commands.registerCommand(command, callback));
 	};
 
-	vscode.chat.createChatParticipant("en-epsanol-chat-particpant", async (_request, _context, response, token) => {
-		response.progress('Let me get back to you on that . . .');
+	vscode.chat.createChatParticipant("en-epsanol-chat-particpant", async (request, _context, response, token) => {
+		response.progress('Gracias por tu paciencia . . .');
 
 		const chatModels = await vscode.lm.selectChatModels({ family: "gpt-4-turbo" });
 
-		const textInEspanol = await askCopilot(`Translate the following file(s) into spanish: ${getTextFromActiveFile()}`, chatModels[0], token);
+		const { baseDirectory, fileContents } = JSON.parse(request.prompt) as FileContentInfo;
 
-		const filenameInEspanol = await askCopilot(`Translate the following into spanish, preserving the extension: ${nameOfCurrentlyOpenFile()}`, chatModels[0], token);
+		const espanolResponse = await askCopilot(`Here are some files provided as JSON: ${JSON.stringify(fileContents)}.` +
+		'Convert contents of files to spanish as well as file names without changing directory or extension.' +
+		'Do not tell me how to do it -- do it for me.', chatModels[0], token);
 
-		response.markdown(`--| ${textInEspanol} |--`);
+		response.markdown("Esta hecho!");
 
-		response.button({ title: 'Write to file', command: 'write-to-file', arguments: [filenameInEspanol, textInEspanol] });
+		response.button({ title: 'Escribir en el archivo', command: 'write-to-file', arguments: [espanolResponse, baseDirectory] });
 	});
 
-	registerCommand('write-to-file', writeTextToFile);
+	registerCommand('write-to-file', writeResponseToFiles);
 	registerCommand('enespanol.in-context-menu', async (uri: vscode.Uri) => {
-		const fileContents = await getContentsFromFiles(uri);
-		vscode.commands.executeCommand('vscode.chat.sendMessage', {
-            participantId: 'en-epsanol-chat-particpant'
-//			context: { fileContents }
-        });
+		const fileContentInfo = await getFileContentInfoFromUri(uri);
+		vscode.commands.executeCommand('workbench.action.chat.open', 
+			`@enespanol ${JSON.stringify(fileContentInfo)}`
+        );
 	});
 }
 
-async function getFilenamesInDirectory(uri: vscode.Uri): Promise<vscode.Uri[]> {
+interface FileContentInfo {
+	baseDirectory?: string;
+	fileContents: FileContent[];
+}
+
+async function getFileContentInfoFromUri(uri: vscode.Uri): Promise<FileContentInfo> {
+	const fileContents = await getContentsFromFiles(uri);
+	const baseDirectory = await isDirectory(uri) ? path.relative(getRootPath(), uri.fsPath) : undefined;
+	fileContents.forEach((fileContent) => {
+		fileContent.fileName = path.relative(baseDirectory ?? '', fileContent.fileName);
+	});
+	return { baseDirectory, fileContents };
+}
+
+async function isDirectory(uri: vscode.Uri): Promise<boolean> {
 	const stat = await vscode.workspace.fs.stat(uri);
-	if (stat.type !== vscode.FileType.Directory) {
+	return stat.type === vscode.FileType.Directory;
+}
+
+async function getFilenamesInDirectory(uri: vscode.Uri): Promise<vscode.Uri[]> {
+	if (!(await isDirectory(uri))) {
 		return [uri];
 	}
     const files = await vscode.workspace.fs.readDirectory(uri);
@@ -76,16 +95,58 @@ function nameOfCurrentlyOpenFile(): string {
 	return path.basename(guranteeDocument().fileName);
 }
 
-async function writeTextToFile(filename: string, text: string) {
-	const workspaceFolders = vscode.workspace.workspaceFolders;
-    if (!workspaceFolders) {
-        vscode.window.showErrorMessage('No workspace folder is open.');
-        return;
-    }
+async function writeResponseToFiles(response: string, baseDirectory: string): Promise<void> {
+	const fileContents = findJsonInString<FileContent[]>(response);
+	await Promise.all(fileContents.map(({ fileName, contents }) => writeTextToFile(fileName, contents, baseDirectory)));
+}
 
-    const rootPath = workspaceFolders[0].uri.fsPath;
-    const filePath = path.join(rootPath, filename);
-    const fileUri = vscode.Uri.file(filePath);
+function findJsonInString<T>(text: string): T {
+    const regex = /```(?:json)?\n([\s\S]*?)\n```/;
+	const jsonAsString = text.match(regex)?.[1] ?? '';
+	return JSON.parse(jsonAsString) as T;
+}
+
+function getRootPath(): string {
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+	if (!workspaceFolders) {
+		throw new Error('No workspace folder is open.');
+	}
+	return workspaceFolders[0].uri.fsPath;
+}
+
+async function createNewDirectory(directoryName: string): Promise<string> {
+	let absoluteDirectoryPath = path.join(getRootPath(), directoryName);
+	let counter = 1;
+
+	while (await directoryExists(absoluteDirectoryPath)) {
+		absoluteDirectoryPath = path.join(getRootPath(), `${directoryName}-${counter}`);
+		counter++;
+	}
+
+	await vscode.workspace.fs.createDirectory(vscode.Uri.file(absoluteDirectoryPath));
+	vscode.window.showInformationMessage(`Directory ${path.basename(absoluteDirectoryPath)} created successfully.`);
+	return path.relative(getRootPath(), absoluteDirectoryPath);
+}
+
+async function directoryExists(directoryPath: string): Promise<boolean> {
+	try {
+		const stat = await vscode.workspace.fs.stat(vscode.Uri.file(directoryPath));
+		return stat.type === vscode.FileType.Directory;
+	} catch {
+		return false;
+	}
+}
+
+async function writeTextToFile(filename: string, text: string, baseDirectory: string): Promise<void> {
+	let absoluteFilename = '';
+	if (baseDirectory) {
+		const newDirectory = await createNewDirectory(baseDirectory);
+		const rootDir = getRootPath();
+		absoluteFilename = path.join(rootDir, newDirectory, filename);
+	} else {
+		absoluteFilename = path.join(getRootPath(), filename);
+	}
+    const fileUri = vscode.Uri.file(absoluteFilename);
 
     const formattedText = formatToWidth(text, 80);
     const encodedText = new TextEncoder().encode(formattedText);
@@ -101,7 +162,7 @@ async function writeTextToFile(filename: string, text: string) {
         }
     }
 
-	openInEditorAndGiveFocus(filePath);
+	openInEditorAndGiveFocus(filename);
 }
 
 function openInEditorAndGiveFocus(filePath: string) {
@@ -143,9 +204,14 @@ async function getContentsFromFiles(uri: vscode.Uri): Promise<FileContent[]> {
 		const fileContents = await Promise.all(files.map((file) => getContentsFromFiles(file)));
 		return fileContents.flat();
 	} else {
-		const contents = new TextDecoder().decode(await vscode.workspace.fs.readFile(uri));
-		return [{ fileName: uri.fsPath, contents }];
+		return [await getContentsFromSingleFile(uri)];
 	}
+}
+
+async function getContentsFromSingleFile(uri: vscode.Uri): Promise<FileContent> {
+	const contents = new TextDecoder().decode(await vscode.workspace.fs.readFile(uri));
+	const fileName = path.relative(getRootPath(), uri.fsPath);
+	return { fileName, contents };
 }
 
 export function deactivate() {}
