138a8ed9a735e3a1b91500ba01b9881c5a648336
Chris Mooney <89551217+cpmooney@users.noreply.github.com>
2024-10-28 07:41:49 -0500

    Rename to smart scaffold

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import { conversationHandler, registerCommands } from './conversation-handler';
 export function activate(context: vscode.ExtensionContext) {
 	registerCommands(context);
 
-	vscode.chat.createChatParticipant("code-from-patterns-chat-participant-chat-particpant", async (request, _context, chatStream, token) => {
+	vscode.chat.createChatParticipant("smart-scaffold-chat-participant-chat-particpant", async (request, _context, chatStream, token) => {
 		const askCopilot = await useAskCopilot(token);
 		await conversationHandler(request.prompt, chatStream, askCopilot);
 	});
